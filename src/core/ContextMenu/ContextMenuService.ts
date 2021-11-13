6909e796b4d7708656b5caa7c959f0f82a28adb6
Karis <sioxas@qq.com>
2021-11-13 12:42:21 +0800

    style(ContextMenu): add comments

--- src/core/ContextMenu/ContextMenuService.ts
+++ src/core/ContextMenu/ContextMenuService.ts
@@ -1,6 +1,5 @@
 import { BehaviorSubject } from "rxjs";
 
-import CanvasService from "../Canvas/CanvasService";
 import Service from "../Service";
 import { MenuPanel, ContextMenuItem } from "./interface";
 import { getAjustedPosition, getMenuHeight } from "./utils";
@@ -38,6 +37,8 @@ export default class ContextMenuService extends Service {
     if (!path.length) return;
     let panels = this.#menuPanels;
     if(panels.length > path.length) {
+      // judge whether the mouse point p is between A and C.
+      // more detail: https://stackoverflow.com/questions/13640931/how-to-determine-if-a-vector-is-between-two-other-vectors
       const p = [event.clientX, event.clientY],
         A = this.#v1, C = this.#v2,
         B = [p[0] - this.#p0[0], p[1] - this.#p0[1]],
@@ -61,13 +62,15 @@ export default class ContextMenuService extends Service {
           [x, y] = getAjustedPosition(rect.right - 8, rect.top, last.children!);
         panels = [...panels, { x, y, options: last.children! }];
         this.#path = path;
+        // compute the intercection point of p1p2 and p3p4
+        // more detail: https://www.cuemath.com/geometry/intersection-of-two-lines/
         const p1 = [event.clientX, rect.top], p2 = [x, y], 
           p3 = [event.clientX, rect.bottom], p4 = [x, y + getMenuHeight(last.children!)],
           a1 = p2[1] - p1[1], b1 = p1[0] - p2[0], c1 = p2[0] * p1[1] - p1[0] * p2[1],
           a2 = p4[1] - p3[1], b2 = p3[0] - p4[0], c2 = p4[0] * p3[1] - p3[0] * p4[1],
           x0 = (b1 * c2 - b2 * c1) / (a1 * b2 - a2 * b1),
           y0 = (a2 * c1 - a1 * c2) / (a1 * b2 - a2 * b1);
-        this.#p0 = [x0, y0];
+        this.#p0 = [x0, y0]; // intersection point
         this.#v1 = [p2[0] - x0, p2[1] - y0];
         this.#v2 = [p4[0] - x0, p4[1] - y0];
       }
