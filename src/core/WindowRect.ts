090318101fd4aba91ac924782f0466d7e218acae
Karis <sioxas@qq.com>
2021-11-14 01:45:12 +0800

    fix(WindowRect): private element

--- src/core/WindowRect.ts
+++ src/core/WindowRect.ts
@@ -3,36 +3,33 @@ export default class Rect {
   #y = 0;
   #w = 0;
   #h = 0;
-  #element: HTMLElement;
   set left(value: number) {
     this.#x = value;
-    this.#element.style.left = `${this.#x}px`;
+    this.element.style.left = `${this.#x}px`;
   }
   get left() {
     return this.#x;
   }
   set top(value: number) {
     this.#y = value;
-    this.#element.style.top = `${this.#y}px`;
+    this.element.style.top = `${this.#y}px`;
   }
   get top() {
     return this.#y;
   }
   set width(value: number) {
     this.#w = value;
-    this.#element.style.width = `${this.#w}px`;
+    this.element.style.width = `${this.#w}px`;
   }
   get width() {
     return this.#w;
   }
   set height(value: number) {
     this.#h = value;
-    this.#element.style.height = `${this.#h}px`;
+    this.element.style.height = `${this.#h}px`;
   }
   get height() {
     return this.#h;
   }
-  constructor(element: HTMLElement) {
-    this.#element = element;
-  }
+  constructor(private element: HTMLElement) {}
 }
