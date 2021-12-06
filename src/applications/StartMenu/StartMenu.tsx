d1bd3a64816ef041838ec850cb087ad9c468657d
Karis <sioxas@qq.com>
2021-12-07 02:02:28 +0800

    feat(StartMenu): add icons

--- src/applications/StartMenu/StartMenu.tsx
+++ src/applications/StartMenu/StartMenu.tsx
@@ -1,11 +1,56 @@
+import Button from '@/components/Button';
 import TextField from '@/components/TextField';
 
 import './style.less';
 
+import start from '@/assets/icons/start.png';
+import search from '@/assets/icons/search-dark.png';
+import widget from '@/assets/icons/widget.png';
+import settings from '@/assets/icons/settings.png';
+import explorer from '@/assets/icons/explorer.png';
+import edge from '@/assets/icons/edge.png';
+import store from '@/assets/icons/store-light.png';
+import vscode from '@/assets/icons/vscode.png';
+
+const apps = [
+  // { name: 'start', icon: start, },
+  // { name: 'search', icon: search, },
+  // { name: 'widget', icon: widget, },
+  { name: 'Settings', icon: settings, },
+  { name: 'Explorer', icon: explorer, },
+  { name: 'Edge', icon: edge, },
+  { name: 'Store', icon: store, },
+  { name: 'VSCode', icon: vscode, },
+];
+
 export default function StartMenuView() {
   return (
-    <div className="start-menu"> 
+    <div className="start-menu">
       <TextField placeholder='在此键入以搜索' size='large' />
+      <div className="pinned-apps">
+        <div className="pinned-apps-header">
+          <div className="pinned-apps-header-title">
+            已固定
+          </div>
+          <div className="pinned-apps-header-more">
+            <Button size="mini">更多</Button>
+          </div>
+        </div>
+        <div className="pinned-apps-content">
+          {
+            apps.map((app) => (
+              <div className="pinned-apps-content-item">
+                <div className="pinned-apps-content-item-icon">
+                  <img src={app.icon} alt={app.name} />
+                </div>
+                <div className="pinned-apps-content-item-title">
+                  {app.name}
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   );
 }
--- src/core/TaskBar/TaskBar.tsx
+++ src/core/TaskBar/TaskBar.tsx
@@ -3,27 +3,7 @@ import classNames from 'classnames';
 
 import { useTaskBarService, useWindowService } from '@/core/ServiceHooks';
 
-import start from '@/assets/icons/start.png';
-import search from '@/assets/icons/search-dark.png';
-import widget from '@/assets/icons/widget.png';
-import settings from '@/assets/icons/settings.png';
-import explorer from '@/assets/icons/explorer.png';
-import edge from '@/assets/icons/edge.png';
-import store from '@/assets/icons/store-light.png';
-import vscode from '@/assets/icons/vscode.png';
-
 import './TaskBar.less';
-
-const apps = [
-  { name: 'start', icon: start, },
-  { name: 'search', icon: search, },
-  { name: 'widget', icon: widget, },
-  { name: 'settings', icon: settings, },
-  { name: 'explorer', icon: explorer, },
-  { name: 'edge', icon: edge, },
-  { name: 'store', icon: store, },
-  { name: 'vscode', icon: vscode, },
-];
 
 export default function TaskBar() {
 
