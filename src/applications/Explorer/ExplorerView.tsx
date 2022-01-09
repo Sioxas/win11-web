c12a9800716fc07f4cd968800c4fdc39a0ee7e6c
Karis <sioxas@qq.com>
2022-01-09 18:58:12 +0800

    fix(Explorer): toolbar style

--- src/applications/Explorer/ExplorerView.tsx
+++ src/applications/Explorer/ExplorerView.tsx
@@ -9,41 +9,41 @@ export default function ExplorerView() {
   return (
     <div className="explorer">
       <div className="explorer-toolbar">
-        <Dropdown.Button type='text' style={{ padding:'0 8px' }} menus={newMenu}>
-          <ColoredIcon glyph={ColoredIconGlyph.New} style={{ fontSize:'18px', margin:'0 5px' }} />
+        <Dropdown.Button type='text' className='explorer-toolbar-item' menus={newMenu}>
+          <ColoredIcon glyph={ColoredIconGlyph.New} className='explorer-toolbar-item-icon' />
           <span>新建</span>
           <i className='iconfont icon-down' style={{ fontSize:'12px', margin: '0 3px' }} />
         </Dropdown.Button>
         <div className="explorer-toolbar-divider"></div>
-        <Button type='text' style={{ padding:'0 8px' }}>
-          <ColoredIcon glyph={ColoredIconGlyph.Cut} style={{ fontSize:'18px', margin:'0 5px' }} />
+        <Button type='text' className='explorer-toolbar-item'>
+          <ColoredIcon glyph={ColoredIconGlyph.Cut} className='explorer-toolbar-item-icon' />
         </Button>
-        <Button type='text' style={{ padding:'0 8px' }}>
-          <ColoredIcon glyph={ColoredIconGlyph.Copy} style={{ fontSize:'18px', margin:'0 5px' }} />
+        <Button type='text' className='explorer-toolbar-item'>
+          <ColoredIcon glyph={ColoredIconGlyph.Copy} className='explorer-toolbar-item-icon' />
         </Button>
-        <Button type='text' style={{ padding:'0 8px' }}>
-          <ColoredIcon glyph={ColoredIconGlyph.CopyLocation} style={{ fontSize:'18px', margin:'0 5px' }} />
+        <Button type='text' className='explorer-toolbar-item'>
+          <ColoredIcon glyph={ColoredIconGlyph.CopyLocation} className='explorer-toolbar-item-icon' />
         </Button>
-        <Button type='text' style={{ padding:'0 8px' }}>
-          <ColoredIcon glyph={ColoredIconGlyph.Paste} style={{ fontSize:'18px', margin:'0 5px' }} />
+        <Button type='text' className='explorer-toolbar-item'>
+          <ColoredIcon glyph={ColoredIconGlyph.Paste} className='explorer-toolbar-item-icon' />
         </Button>
-        <Button type='text' style={{ padding:'0 8px' }}>
-          <ColoredIcon glyph={ColoredIconGlyph.Rename} style={{ fontSize:'18px', margin:'0 5px' }} />
+        <Button type='text' className='explorer-toolbar-item'>
+          <ColoredIcon glyph={ColoredIconGlyph.Rename} className='explorer-toolbar-item-icon' />
         </Button>
-        <Button type='text' style={{ padding:'0 8px' }}>
-          <ColoredIcon glyph={ColoredIconGlyph.Share} style={{ fontSize:'18px', margin:'0 5px' }} />
+        <Button type='text' className='explorer-toolbar-item'>
+          <ColoredIcon glyph={ColoredIconGlyph.Share} className='explorer-toolbar-item-icon' />
         </Button>
-        <Button type='text' style={{ padding:'0 8px' }}>
-          <ColoredIcon glyph={ColoredIconGlyph.Delete} style={{ fontSize:'18px', margin:'0 5px' }} />
+        <Button type='text' className='explorer-toolbar-item'>
+          <ColoredIcon glyph={ColoredIconGlyph.Delete} className='explorer-toolbar-item-icon' />
         </Button>
         <div className="explorer-toolbar-divider"></div>
-        <Button type='text' style={{ padding:'0 8px' }}>
-          <ColoredIcon glyph={ColoredIconGlyph.SortBy} style={{ fontSize:'18px', margin:'0 5px' }} />
+        <Button type='text' className='explorer-toolbar-item'>
+          <ColoredIcon glyph={ColoredIconGlyph.SortBy} className='explorer-toolbar-item-icon' />
           <span>排序</span>
           <i className='iconfont icon-down' style={{ fontSize:'12px', margin: '0 3px' }} />
         </Button>
-        <Button type='text' style={{ padding:'0 8px' }}>
-          <ColoredIcon glyph={ColoredIconGlyph.Properties} style={{ fontSize:'18px', margin:'0 5px' }} />
+        <Button type='text' className='explorer-toolbar-item'>
+          <ColoredIcon glyph={ColoredIconGlyph.Properties} className='explorer-toolbar-item-icon' />
         </Button>
       </div>
     </div>
