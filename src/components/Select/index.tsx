32fcdd4e3f6321eecbd5ba19b7ebe65ba252e120
Sioxas <sioxas@qq.com>
2022-02-09 09:23:36 +0000

    fea(Select): SelectOptionsPositionStrategy

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,9 +1,8 @@
-import React, { cloneElement, isValidElement, ReactElement, ReactNode, ReactNodeArray, RefObject, useRef } from 'react';
+import React, { cloneElement, isValidElement, ReactElement, ReactNode, ReactNodeArray, useRef } from 'react';
 import classNames from 'classnames';
 
-import Point from '@/utils/Point';
 import { useOverlay } from '@/core/ServiceHooks';
-import { PositionStrategy } from '@/core/Overlay/PositioinStrategy';
+import SelectOptionsPositionStrategy from './SelectOptionsPositionStrategy';
 import Button from '../Button';
 
 import './style.less';
@@ -61,7 +60,7 @@ export function Select<T>(props: SelectProps<T>) {
     if (value) {
       const optionIndex = options.findIndex(option => compareWith(option.props.value, value));
       if (optionIndex !== -1) {
-        positionStrategy.withAlignedOptionIndex(optionIndex);
+        positionStrategy.alignWith(optionIndex);
       }
     }
 
@@ -109,105 +108,3 @@ function defaultCompareFn<T>(a: T, b: T) {
   return a === b;
 }
 
-class SelectOptionsPositionStrategy implements PositionStrategy {
-
-  private overlayRef?: RefObject<HTMLElement>
-
-  private optionIndex = 0;
-
-  private verticalFlexible = false;
-
-  private horizontalFlexible = false;
-
-  private offset = new Point(0, 0);
-
-  constructor(private origin: RefObject<HTMLButtonElement>) { }
-
-  withAlignedOptionIndex(index: number) {
-    this.optionIndex = index;
-    return this;
-  }
-
-  withVertivalFlexible(verticalFlexible = true) {
-    this.verticalFlexible = verticalFlexible;
-    return this;
-  }
-
-  withHorizontalFlexible(horizontalFlexible = true) {
-    this.horizontalFlexible = horizontalFlexible;
-    return this;
-  }
-
-  withOffset(offsetX: number, offsetY: number) {
-    this.offset = { x: offsetX, y: offsetY };
-    return this;
-  }
-
-  attach(overlayRef: RefObject<HTMLElement>): void {
-    this.overlayRef = overlayRef;
-  }
-
-  apply(): void {
-    if (!this.origin.current || !this.overlayRef?.current) {
-      throw new Error("origin and overlayRef must be set before apply position strategy");
-    }
-
-    let
-      originRect = this.origin.current.getBoundingClientRect(),
-      overlayRect = this.overlayRef.current.getBoundingClientRect();
-
-    const overlayPosition = this.getOverlayPosition(this.overlayRef.current, overlayRect, originRect);
-    this.adjustOverlayPosition(overlayPosition, overlayRect);
-    this.applyPosition(overlayPosition);
-  }
-
-  dispose(): void {
-  }
-
-  private getOverlayPosition(overlayElement: HTMLElement, overlayRect: DOMRect, originRect: DOMRect): Point {
-    let
-      index = this.optionIndex >= 0 && this.optionIndex < overlayElement.children.length ? this.optionIndex : 0,
-      option = overlayElement.children[index],
-      optionRect = option.getBoundingClientRect(),
-      /** Position of option relative to overlay */
-      optionRelativePosition = new Point(optionRect.left - overlayRect.left, optionRect.top - overlayRect.top);
-
-    return new Point(
-      originRect.left - optionRelativePosition.x + this.offset.x,
-      originRect.top - optionRelativePosition.y + this.offset.y
-    );
-  }
-
-  private adjustOverlayPosition(overlayPoint: Point, overlayRect: DOMRect) {
-    const { width, height } = overlayRect;
-    if (this.horizontalFlexible) {
-      if (overlayPoint.x < 0) {
-        overlayPoint.x = 0;
-      } else if (overlayPoint.x + width > window.innerWidth) {
-        overlayPoint.x = window.innerWidth - width;
-      }
-    }
-    if (this.verticalFlexible) {
-      if (overlayPoint.y < 0) {
-        overlayPoint.y = 0;
-      } else if (overlayPoint.y + height > window.innerHeight) {
-        overlayPoint.y = window.innerHeight - height;
-      }
-    }
-  }
-
-  /**
-   * Applies a computed position to the overlay.
-   * @param overlayPoint The overlay element start point.
-   */
-  private applyPosition(overlayPoint: Point) {
-    const overlayElement = this.overlayRef?.current;
-    if (overlayElement) {
-      let { x, y } = overlayPoint;
-      overlayElement.style.position = 'absolute';
-      overlayElement.style.zIndex = '1000';
-      overlayElement.style.left = `${x}px`;
-      overlayElement.style.top = `${y}px`;
-    }
-  }
-}
